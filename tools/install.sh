e4a208c17b6b6aba389a225254461184d7a018f6
Martin Schneider <martin.schneider@pludoni.de>
2012-01-05 23:15:41 +0100

    install update

--- tools/install.sh
+++ tools/install.sh
@@ -5,7 +5,7 @@ then
 fi
 
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-/usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+/usr/bin/env git clone https://github.com/pludoni/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
@@ -24,13 +24,16 @@ echo "export PATH=$PATH" >> ~/.zshrc
 echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
 chsh -s `which zsh`
 
-echo "\033[0;32m"'         __                                     __   '"\033[0m"
-echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
-echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
-echo "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"\033[0m"
-echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m"
-echo "\033[0;32m"'                        /____/                       '"\033[0m"
+echo "\033[0;32m"'       ___                __                         '"\033[0m"
+echo "\033[0;32m"'      /\_ \              /\ \                  __    '"\033[0m"
+echo "\033[0;32m"' _____\//\ \    __  __   \_\ \    ___     ___ /\_\   '"\033[0m"
+echo "\033[0;32m"'/\  __`\\ \ \  /\ \/\ \  / __ \  / __`\ /  _ `\/\ \  '"\033[0m"
+echo "\033[0;32m"'\ \ \_\ \\_\ \_\ \ \_\ \/\ \_\ \/\ \_\ \/\ \/\ \ \ \ '"\033[0m"
+echo "\033[0;32m"' \ \ ,__//\____\\ \____/\ \___,_\ \____/\ \_\ \_\ \_\'"\033[0m"
+echo "\033[0;32m"'  \ \ \/ \/____/ \/___/  \/__,_ /\/___/  \/_/\/_/\/_/'"\033[0m"
+echo "\033[0;32m"'   \ \_\                                             '"\033[0m"
+echo "\033[0;32m"'    \/_/                                             '"\033[0m"
 
-echo "\n\n \033[0;32m....is now installed.\033[0m"
+echo "\n\n \033[0;32m ..version of oh-my-zsh is now installed.\033[0m"
 /usr/bin/env zsh
 source ~/.zshrc
