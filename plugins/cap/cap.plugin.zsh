f13e24e69fda01eacfd0e1c3cac8559e87ae3793
Martin Schneider <martin.schneider@pludoni.de>
2012-01-19 17:09:35 +0100

    cap was broken

--- plugins/cap/cap.plugin.zsh
+++ plugins/cap/cap.plugin.zsh
@@ -1,4 +1,4 @@
-function _cap_does_task_list_need_generating () {
+_cap_does_task_list_need_generating () {
   if [ ! -f .cap_tasks~ ]; then return 0;
   else
     accurate=$(stat -c%Z .cap_tasks~)
@@ -7,15 +7,14 @@ function _cap_does_task_list_need_generating () {
   fi
 }
 
-function _cap () {
+_cap () {
   if [ -f config/deploy.rb ]; then
     if _cap_does_task_list_need_generating; then
-      echo "\nGenerating .cap_tasks~..." > /dev/stderr
-      cap show_tasks -q | cut -d " " -f 1 | sed -e '/^ *$/D' -e '1,2D'
-> .cap_tasks~
+     echo "\nGenerating .cap_tasks~..." > /dev/stderr
+     bundle exec cap -T -q | grep '#' | cut -d ' ' -f 2 | sort > .cap_tasks~
     fi
     compadd `cat .cap_tasks~`
   fi
 }
 
-compctl -K _cap cap
+compdef _cap cap
